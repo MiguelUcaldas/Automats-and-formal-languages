758df931a394259dd4f3cd863850c4316502e219
vogel <over.castrillon22396@ucaldas.edu.co>
2023-02-18 13:34:13 -0500

    Program loading and user interface, files needs to be ordered, models from python also has been created.

--- app.js
+++ app.js
@@ -1,55 +1,64 @@
-// Obtener el botón de carga de archivos
-var fileInput = document.getElementById("fileInput");
+document.addEventListener("DOMContentLoaded", init)
 
-// Agregar un evento al botón de carga de archivos
-fileInput.addEventListener("change", function (event) {
-    // Obtener el archivo seleccionado
-    var file = event.target.files[0];
+async function init() {
+    // enter()
+    // read()
+    // let program = new Control()
 
-    // Crear un lector de archivos
-    var reader = new FileReader();
+    // program.optionsMenu()
+    // Obtener el botón de carga de archivos
+    let fileInput = document.getElementById("fileInput");
 
-    // Agregar un evento para cuando el archivo se cargue correctamente
-    reader.addEventListener("load", function (event) {
-        // Obtener los datos del archivo JSON
-        var data = JSON.parse(event.target.result);
+    // Agregar un evento al botón de carga de archivos
+    fileInput.addEventListener("change", function (event) {
+        // Obtener el archivo seleccionado
+        let file = event.target.files[0];
 
-        // Generar el diagrama GoJS a partir de los datos del archivo JSON
-        generateDiagram(data);
+        // Crear un lector de archivos
+        let reader = new FileReader();
+
+        // Agregar un evento para cuando el archivo se cargue correctamente
+        reader.addEventListener("load", function (event) {
+            // Obtener los datos del archivo JSON
+            let data = JSON.parse(event.target.result);
+
+            // Generar el diagrama GoJS a partir de los datos del archivo JSON
+            generateDiagram(data);
+        });
+
+        // Leer el archivo como texto
+        reader.readAsText(file);
     });
 
-    // Leer el archivo como texto
-    reader.readAsText(file);
-});
+    // Función para generar el diagrama GoJS a partir de los datos del archivo JSON
+    function generateDiagram(data) {
+        // Crear el diagrama GoJS
+        let $ = go.GraphObject.make;
+        let myDiagram = $(go.Diagram, "myDiagramDiv");
 
-// Función para generar el diagrama GoJS a partir de los datos del archivo JSON
-function generateDiagram(data) {
-    // Crear el diagrama GoJS
-    var $ = go.GraphObject.make;
-    var myDiagram = $(go.Diagram, "myDiagramDiv");
+        // Crear los nodos a partir de los datos del archivo JSON
+        let nodeDataArray = [];
+        data.nodes.forEach(function (node) {
+            nodeDataArray.push({ key: node.key, color: node.color });
+        });
 
-    // Crear los nodos a partir de los datos del archivo JSON
-    var nodeDataArray = [];
-    data.nodes.forEach(function (node) {
-        nodeDataArray.push({ key: node.key, color: node.color });
-    });
+        // Crear las relaciones a partir de los datos del archivo JSON
+        let linkDataArray = [];
+        data.links.forEach(function (link) {
+            linkDataArray.push({ from: link.from, to: link.to, text: link.text });
+        });
 
-    // Crear las relaciones a partir de los datos del archivo JSON
-    var linkDataArray = [];
-    data.links.forEach(function (link) {
-        linkDataArray.push({ from: link.from, to: link.to, text: link.text });
-    });
-
-    // Configurar el modelo de datos del diagrama
-    var model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
-    myDiagram.model = model;
+        // Configurar el modelo de datos del diagrama
+        let model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
+        myDiagram.model = model;
+    }
 }
-// // Crear el diagrama
-// var $ = go.GraphObject.make;  // Acceder al constructor de objetos GoJS
-// var myDiagram =
-//     $(go.Diagram, "myDiagramDiv",
-//         {
-//             // Configurar el diagrama
+    // Crear el diagrama
+    // let $ = go.GraphObject.make;  // Acceder al constructor de objetos GoJS
+    // let myDiagram =
+    //     $(go.Diagram, "myDiagramDiv",
+    //         {
+        //             // Configurar el diagrama
 //             "undoManager.isEnabled": true // Permite deshacer y rehacer cambios
 //         });
 
@@ -78,7 +87,7 @@ function generateDiagram(data) {
 //             new go.Binding("text", "key"))
 //     );
 
-// var model = $(go.GraphLinksModel);
+// let model = $(go.GraphLinksModel);
 
 // model.nodeDataArray = [
 //     { key: "Nodo 1" },
